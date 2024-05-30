019d1cdc5c4a98ef2c6b91be9aa93ba9d27530c2
Gajender <Iamsanju0707@gmail.com>
2024-05-30 14:01:53 +0530

    renaming the file from main to geeksforgeeks_pattern_questions

---
+++ src/geeksforgeeks_pattern_Questions.java
@@ -0,0 +1,26 @@
+public class geeksforgeeks_pattern_Questions {
+    public static void main(String[] args) {
+
+
+        //1. Number triangle Pattern:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
