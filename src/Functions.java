3ec3bea3c85141292cabbb8215e46792fd06b67e
Gajender <Iamsanju0707@gmail.com>
2024-06-04 13:00:44 +0530

    modifiying the files

--- src/Functions.java
+++ src/Functions.java
@@ -3,14 +3,28 @@ import java.util.Scanner;
 public class Functions {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
-        System.out.println("Enter the n: ");
-        int n=scn.nextInt();
-        System.out.println("Enter the r: ");
-        int r=scn.nextInt();
-        int result=factorial(n)/(factorial(r)*factorial(n-r));
-        System.out.println("The result of ncr  is " + result);
+//        System.out.println("Enter the n: ");
+//        int n=scn.nextInt();
+//        System.out.println("Enter the r: ");
+//        int r=scn.nextInt();
+//        int result=factorial(n)/(factorial(r)*factorial(n-r));
+//        System.out.println("The result of ncr  is " + result);
+//        int res=sum(2,39);
+        System.out.println("Enter the Number: ");
+        int num= scn.nextInt();
+        int res=add(num,cube(num));
+        System.out.println(res);
+
+
 
     }
+    public static int cube(int a){
+        return a*a*a;
+    }
+    public static int add(int n1, int n2){
+        return n1+n2;
+    }
+
 
     public static int factorial( int num){
         int fact=1;
@@ -19,5 +33,10 @@ public class Functions {
         }
         return fact;
 
+    }
+
+    public static int sum(int a, int b){
+        int add =a+b;
+        return add;
     }
 }
