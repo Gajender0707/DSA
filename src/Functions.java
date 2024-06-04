c2ded81e69927b71749b390edaf9015deb7f30c6
Gajender <Iamsanju0707@gmail.com>
2024-06-04 11:48:44 +0530

    modifying the file

--- src/Functions.java
+++ src/Functions.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class Functions {
     public static void main(String[] args) {
-        System.out.println("Enter the Number for Factorial: ");
         Scanner scn=new Scanner(System.in);
+        System.out.println("Enter the n: ");
         int n=scn.nextInt();
-        int f=factorial(n);
-        System.out.println("Factorial is " + f);
+        System.out.println("Enter the r: ");
+        int r=scn.nextInt();
+        int result=factorial(n)/(factorial(r)*factorial(n-r));
+        System.out.println("The result of ncr  is " + result);
 
     }
 
@@ -14,7 +16,6 @@ public class Functions {
         int fact=1;
         for(int i=1; i<=num; i++){
             fact=fact*i;
-
         }
         return fact;
 
