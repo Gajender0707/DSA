f309832d72d499b042edfb22b64f990d71ddfead
Gajender <Iamsanju0707@gmail.com>
2024-06-05 12:24:23 +0530

    modifying the file

--- src/geeksforgeeks_pattern_Questions.java
+++ src/geeksforgeeks_pattern_Questions.java
@@ -1,13 +1,116 @@
+import java.util.Scanner;
 public class geeksforgeeks_pattern_Questions {
     public static void main(String[] args) {
 
+        // Ques1: Number Triangle Pattern.
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter the Number: ");
+//        int n = scn.nextInt();
+//        int count = 1;
+//        int nsp = n - 1;
+//        for (int i = 1; i <= n; i++) {
+//            for (int k = 1; k <= nsp; k++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 1; j <= count; j++) {
+//                System.out.print(count + " ");
+//            }
+//            count++;
+//            nsp = nsp - 1;
+//            System.out.println();
 
-        //1. Number triangle Pattern:
+
+            //3. Number-increasing Pyramid Pattern
+
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter the Number: ");
+//        int n = scn.nextInt();
+//
+//        int count=1;
+//        for(int i=1;i<=n; i++) {
+//            int num=1;
+//            for (int j = 1; j <= count; j++) {
+//                System.out.print(num+" ");
+//                num++;
+//
+//            }
+//            count++;
+//            System.out.println();
+//        }
 
 
 
 
 
+
+        //4. Number-increasing reverse Pyramid Pattern
+
+//                Scanner scn = new Scanner(System.in);
+//                System.out.println("Enter the Number: ");
+//                int n = scn.nextInt();
+//                int count=n;
+//                for(int i=1; i<=n; i++){
+//                    int num=1;
+//                    for(int j=1; j<=count; j++){
+//                        System.out.print(num+" ");
+//                        num++;
+//                    }
+//                    count--;
+//                    System.out.println();
+//
+//                }
+//
+
+
+
+//        5. Number-changing Pyramid Pattern
+//                Scanner scn = new Scanner(System.in);
+//                System.out.println("Enter the Number: ");
+//                int n = scn.nextInt();
+//                int count=1;
+//                int num=1;
+//                for(int i=1; i<=n; i++){
+//                    for(int j=1; j<=count;j++){
+//                        System.out.print(num+" ");
+//                        num++;
+//                    }
+//                    count++;
+//                    System.out.println();
+//                }
+
+
+
+
+
+        //6.Zero-One Triangle Pattern
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter the Number: ");
+//        int n = scn.nextInt();
+//        int count=1;
+//
+//        for(int i=1; i<=n; i++){
+//            for(int j=1; j<=count; j++){
+//                if(count%2==0){
+//                    if(j%2==0){
+//                        System.out.print(1+" ");
+//                    }
+//                    else{
+//                        System.out.print(0+" ");
+//                    }
+//                }
+//                else{
+//                    if(j%2==0){
+//                        System.out.print(0+" ");
+//                    }
+//                    else{
+//                        System.out.print(1+" ");
+//                    }
+//                }
+//
+//            }
+//            count++;
+//            System.out.println();
+//        }
 
 
 
