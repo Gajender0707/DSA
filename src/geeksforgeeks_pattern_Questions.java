eed33ab9304cf87e68dc9c481d5969fa8411ac73
Gajender <Iamsanju0707@gmail.com>
2024-06-06 10:50:22 +0530

    adding the questions

--- src/geeksforgeeks_pattern_Questions.java
+++ src/geeksforgeeks_pattern_Questions.java
@@ -106,34 +106,99 @@ public class geeksforgeeks_pattern_Questions {
 
 
         //7. Palindrome Triangle Pattern
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter the Number: ");
+//        int n = scn.nextInt();
+//        int count = 1;
+//        int nsp = n-1;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            for (int k = 1; k <= nsp; k++) {
+//                System.out.print(" ");
+//            }
+//            int num1=count;
+//            for (int j = 1; j <= count; j++) {
+//
+//                System.out.print(num1);
+//                num1--;
+//
+//        }
+//            int num2=2;
+//            for(int l=2;l<=count; l++){
+//                System.out.print(num2);
+//                num2++;
+//            }
+//
+//        count = count + 1;
+//        nsp = nsp - 1;
+//        System.out.println();
+//    }
+
+
+        // 8. Rhombus Pattern
+
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter the Number: ");
+//        int n = scn.nextInt();
+//        int nsp=n-1;
+//        int nst=n;
+//        for(int i=1; i<=n; i++){
+//            for(int j=1;j<=nsp; j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=1;j<=nst; j++){
+//                System.out.print("*");
+//            }
+//
+//            nsp--;
+//            System.out.println();
+//
+//        }
+
+
+
+
+        //9. Diamond Star Pattern
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter the Number: ");
+//        int n = scn.nextInt();
+//        int nsp=n/2;
+//        int nst=1;
+//
+//        for(int i=1; i<=n; i++){
+//            if(n/2>=i){
+//                for(int j=1; j<=nsp; j++){
+//                    System.out.print(" ");
+//                }
+//                for(int k=1; k<=nst; k++){
+//                    System.out.print("*");
+//                }
+//                nsp--;
+//                nst=nst+2;
+//                System.out.println();
+//            }
+//            else{
+//                for(int j=1; j<=nsp; j++){
+//                    System.out.print(" ");
+//                }
+//                for(int k=1; k<=nst; k++){
+//                    System.out.print("*");
+//                }
+//                nsp++;
+//                nst=nst-2;
+//                System.out.println();
+//            }
+//        }
+
+
+
+
+        //10. Butterfly Star Pattern
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter the Number: ");
         int n = scn.nextInt();
-        int count = 1;
-        int nsp = n-1;
 
-        for (int i = 1; i <= n; i++) {
-
-            for (int k = 1; k <= nsp; k++) {
-                System.out.print(" ");
-            }
-            int num1=count;
-            for (int j = 1; j <= count; j++) {
-
-                System.out.print(num1);
-                num1--;
-
-        }
-            int num2=2;
-            for(int l=2;l<=count; l++){
-                System.out.print(num2);
-                num2++;
-            }
-
-        count = count + 1;
-        nsp = nsp - 1;
-        System.out.println();
-    }
 
 
 
